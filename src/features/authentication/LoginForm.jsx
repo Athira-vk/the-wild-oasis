a5b7632a184d77bc5d19dabb975510556dbc26c8
Athira-vk <144921132+Athira-vk@users.noreply.github.com>
2024-12-07 20:45:55 +0530

    Update LoginForm.jsx
    change the password

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -8,8 +8,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("athira@example.com");
+  const [password, setPassword] = useState("athiravk");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
